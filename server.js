d9d502de42173e719641e7d08111bf8b05b52df7
William Takes <william.e.takes@vanderbilt.edu>
2021-07-04 17:24:16 -0400

    require paths and PORT established

---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require("express");
+var path = require("path");
+var fs = require("fs")
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
